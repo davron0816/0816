45021987e7d3157ad9d352b9d750c4f84899496f
davron0816 <davronergawev0816@gmail.com>
2025-01-07 18:23:29 +0500

    Add files via upload

---
+++ 1Уй иши/script.js
@@ -0,0 +1,6 @@
+let Name  = prompt(`Ismingizni kiriting`);
+let Fam = prompt(`Familyangizni kiriting`);
+let age = prompt(`Yoshingizni kiriting`);
+let fans =prompt(`Yoqtirgan faningizni kiriting`);
+let loc =prompt(`Yashash manzilingizni kiriting`);
+alert (`Malumot uchun raxmat!`)
